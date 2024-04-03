67e992e4d4ff1b81fab45d6210ea7b51709df81c
Abbypong11 <abbypong11@gmail.com>
2024-04-03 12:03:55 +0000

    home page, about page and social links added

--- personal-portfolio/src/App.js
+++ personal-portfolio/src/App.js
@@ -1,10 +1,19 @@
+import About from "./components/About";
+import Home from "./components/Home";
 import Navbar from "./components/navbar";
+import SocialLinks from "./components/SocialLinks";
+
 
 
 function App() {
   return (
 <div>
 <Navbar/> 
+<Home/>
+<About/>
+
+
+<SocialLinks/>
 </div>
   );
 }
